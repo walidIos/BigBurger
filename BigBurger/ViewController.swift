71225bf107a77b6312aa103d0fb8c6a14d93bb85
walidIos <walid.nakbi91@gmail.com>
2022-09-07 00:08:14 +0100

    first commit

---
+++ BigBurger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BigBurger
+//
+//  Created by walid on 7/9/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
